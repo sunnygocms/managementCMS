4d8b5948fc2041a2bfcdf61321337c8ea6eb3e47
sunnyregion <jinheking@sina.com>
2016-05-11 13:01:45 +0800

    create bee project

---
+++ main.go
@@ -0,0 +1,11 @@
+package main
+
+import (
+	_ "github.com/sunnygocms/managementCMS/routers"
+	"github.com/astaxie/beego"
+)
+
+func main() {
+	beego.Run()
+}
+
---
+++ routers/router.go
@@ -0,0 +1,10 @@
+package routers
+
+import (
+	"github.com/sunnygocms/managementCMS/controllers"
+	"github.com/astaxie/beego"
+)
+
+func init() {
+    beego.Router("/", &controllers.MainController{})
+}
